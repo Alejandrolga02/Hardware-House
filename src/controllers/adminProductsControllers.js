06d75c9857e71297ad6f48d097c1ecc366d64353
Jesús López <2020030232@upsin.edu.mx>
2023-03-16 19:02:16 -0700

    Temporary file deletion

--- src/controllers/adminProductsControllers.js
+++ src/controllers/adminProductsControllers.js
@@ -1,8 +1,9 @@
 import { pool } from "../db.js";
 import cloudinary from '../cloudinary.js';
+import fs from "fs";
 
 
-
+//Función para mostrar todos los productos.
 export const renderProducts = async (req, res) => {
 	const [rows] = await pool.query("SELECT productos.codigo, productos.nombre, productos.descripcion, productos.precio, productos.urlImagen, productos.disponibilidad, productos.idCategoria, categorias.nombre AS categoria FROM productos LEFT JOIN categorias ON productos.idCategoria = categorias.id");
 	const [categorias] = await pool.query("SELECT * FROM categorias");
@@ -18,6 +19,7 @@ export const renderProducts = async (req, res) => {
 	});
 };
 
+//Función para crear un nuevo producto.
 export const createProducts = async (req, res) => {
 	try {
 		const photo = req.files.urlImagen;	//Se obtiene el objeto del archivo
@@ -25,6 +27,15 @@ export const createProducts = async (req, res) => {
 			folder: "products",
 		});
 		const url = result.url;	//Se obtienenla URL de la imagen en Cloudinary
+
+		//Función para eliminar las imagenes temporales.
+		try{
+			fs.unlinkSync(photo.tempFilePath);
+			console.log("Archivo removido");
+		}catch(err){
+			console.error("Error ", err);
+		}
+
 		const newProduct = {	//Creación del objeto usado para realizar la inserción
 			codigo: req.body.codigo,
 			nombre: req.body.nombre,
@@ -36,8 +47,8 @@ export const createProducts = async (req, res) => {
 			idCategoria: parseInt(req.body.idCategoria)
 		}
 		console.log(newProduct);
-		const rows = await pool.query("INSERT INTO productos set ?", [newProduct]);
-		res.redirect("/admin/productos");
+		const rows = await pool.query("INSERT INTO productos set ?", [newProduct]);	//Se realiza la inserción.
+		res.redirect("/admin/productos");	//Se redirecciona a la pagina de productos
 		return;
 		
 	} catch (error) {
@@ -63,18 +74,22 @@ export const updateProducts = async (req, res) => {
 	res.redirect("/admin/productos");
 };
 
+//Función para desactivar los productos.
 export const deleteProducts = async (req, res) => {
 	const { id } = req.params;
-	//console.log(req.params);
-	const [rows] = await pool.query("SELECT estado FROM productos WHERE codigo = ?", [id]);
-	//console.log(rows[0].estado);	
+	const [rows] = await pool.query("SELECT estado FROM productos WHERE codigo = ?", [id]);	
 	const estado = rows[0].estado;	//obtención del estado.
+
 	if(estado == 1){	//Se realiza el cambio del estado.
 		const result = await pool.query("UPDATE productos set estado = ? WHERE codigo = ?", [0, id]);
 		console.log("SE DESACTIVO")
+		console.log(result);
 	}else{
 		const result = await pool.query("UPDATE productos set estado = ? WHERE codigo = ?", [1, id]);
 		console.log("SE ACTIVO");
+		console.log(result);
 	}
 	res.redirect("/admin/productos");
 };
+
+
