8d39f9c1c44ad5e791701e5772e4c2fa403877f8
Alejandro Iván Lizárraga Rojas <alejandroivanliza@gmail.com>
2023-03-24 18:05:05 -0700

    Documentación del proyecto

---
+++ README.md
@@ -0,0 +1,73 @@
+# Hardware House
+
+Proyecto de un E-Commerce en Nodejs con Express, Maquetado con ejs, base de datos MySQL con mysql2 y Imagenes subidas a cloudinary
+
+Este proyecto está conformado por dos partes, la primer parte es para el usuario donde este puede consultar los productos disponibles y comprarlos a través de un carrito.  
+Y la segunda es un panel de administrador donde puede agregar productos, categorias, promociones y consultar las ventas.
+
+## Enlace de proyecto en la nube
+
+-   [Cliente](http://alejandrolizarraga.tech/)
+-   [Administrador](http://alejandrolizarraga.tech/admin/auth)
+
+## Tecnologias usadas
+
+-   [Nodejs](https://nodejs.org/en)
+-   [Git](https://git-scm.com/)
+-   [MySQL](https://www.mysql.com/)
+-   [Visual Studio Code](https://code.visualstudio.com/)
+-   [Cloudinary](https://cloudinary.com/)
+-   [AWS](https://aws.amazon.com/es/)
+
+## Instalación local
+
+1. Clonar el repositorio
+
+```
+git clone https://gitlab.com/gadaca/hardware-house.git
+```
+
+2. Instalar los paquetes necesarios
+
+```
+npm install
+```
+
+3. Crear archivo .env usado para guardar las variables de entorno
+
+```
+cp .env.example .env
+```
+
+4. Crear la base de datos en mysql a partir del schema.sql
+
+5. Guardar las credenciales de la base de datos en el .env
+
+```
+DB_HOST = "your_db_host"
+DB_USER = "your_user"
+DB_PASS = "db_password"
+DB_PORT = "db_port"
+DB_NAME = "HardwareHouse"
+```
+
+6. Iniciar el proyecto
+
+```
+npm run dev
+```
+
+7. Accedemos a la pagina
+
+```
+http://localhost/
+```
+
+## Desarrollado por
+
+1. [Alejandro Iván Lizárraga Rojas](https://gitlab.com/Alejandrolga02)
+2. [Jesus Daniel Lopez Robles](https://gitlab.com/JesusLopez117)
+3. [Cesar Oswaldo Bernal Sanchez](https://gitlab.com/CesarUPSIN)
+4. [Guillermo Manuel Sanchez Lizarraga](https://gitlab.com/GuillermoSan02)
+5. [Marvin Alejandro Lopez Diaz](https://gitlab.com/MarvinLopezDiaz)
+6. [Jose Manuel Cerezo Arreola](https://gitlab.com/2018030209)
---
+++ SCHEMA.sql
@@ -0,0 +1,155 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 35.167.76.213    Database: HardwareHouse
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.22.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(60) NOT NULL,
+  `contrasena` varchar(161) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `estado` tinyint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comprador`
+--
+
+DROP TABLE IF EXISTS `comprador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comprador` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  `apellidos` varchar(60) NOT NULL,
+  `correo` varchar(60) NOT NULL,
+  `contrasena` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `codigo` varchar(20) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `urlImagen` varchar(120) NOT NULL,
+  `estado` tinyint NOT NULL,
+  `disponibilidad` int NOT NULL,
+  `idCategoria` int NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `idCategoria_idx` (`idCategoria`),
+  CONSTRAINT `idCategoria` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `promociones`
+--
+
+DROP TABLE IF EXISTS `promociones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promociones` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fechaInicio` date NOT NULL,
+  `fechaFin` date NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `porcentajeDescuento` decimal(10,0) NOT NULL,
+  `idCategoria` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idCategoriaProm_idx` (`idCategoria`),
+  CONSTRAINT `idCategoriaProm` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ventas`
+--
+
+DROP TABLE IF EXISTS `ventas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ventas` (
+  `id` int NOT NULL,
+  `idVendedor` int NOT NULL,
+  `idComprador` int NOT NULL,
+  `fecha` datetime NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idComprador_idx` (`idComprador`),
+  KEY `idVendedor_idx` (`idVendedor`),
+  CONSTRAINT `idComprador` FOREIGN KEY (`idComprador`) REFERENCES `comprador` (`id`),
+  CONSTRAINT `idVendedor` FOREIGN KEY (`idVendedor`) REFERENCES `admin` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ventas_detalle`
+--
+
+DROP TABLE IF EXISTS `ventas_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ventas_detalle` (
+  `idVenta` int NOT NULL,
+  `idProducto` varchar(20) NOT NULL,
+  `cantidad` decimal(10,2) NOT NULL,
+  KEY `idVentas_idx` (`idVenta`),
+  KEY `idProducto_idx` (`idProducto`),
+  CONSTRAINT `idProducto` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`codigo`),
+  CONSTRAINT `idVentas` FOREIGN KEY (`idVenta`) REFERENCES `ventas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-24 17:56:38
