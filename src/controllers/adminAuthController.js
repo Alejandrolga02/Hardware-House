93e2b00d859f2537482576a2297cbcc7bac98dd3
Guillermo Sanchez <2020030309@upsin.edu.mx>
2023-04-08 23:14:58 -0700

    Funcion de registrar creada

--- src/controllers/adminAuthController.js
+++ src/controllers/adminAuthController.js
@@ -1,6 +1,7 @@
 import { pool } from "../db.js";
 import { generarJWT } from "../jwt.js";
-import { scryptSync, randomBytes, timingSafeEqual } from 'crypto'
+import { scryptSync, randomBytes, timingSafeEqual } from 'crypto';
+import estados from '../estados.js';
 
 export const login = async (req, res) => {
     try {
@@ -114,18 +115,86 @@ export const renderRegister = async (req, res) => {
     }
 }
 
+const validateUser = newUser => {
+    try {
+        let { usuario, contrasena, nombre, apellidos, correo, telefono, calle, colonia, numeroExterior, CP, estado, municipio } = newUser;
+
+        // Usuario vacio
+        if (!usuario || usuario.length === 0 || usuario.length > 60)
+            return false;
+
+        // Contraseña vacia
+        if (!contrasena || contrasena.length === 0 || contrasena.length > 60)
+            return false;
+
+        // nombre vacio
+        if (!nombre || nombre.length === 0 || nombre.length > 60)
+            return false;
+
+        // correo vacio
+        if (!correo || correo.length === 0 || correo.length > 240)
+            return false;
+
+        // apellido vacio
+        if (!apellidos || apellidos.length === 0 || apellidos.length > 60)
+            return false;
+
+        // telefono vacio
+        if (!telefono || telefono.length === 0 || telefono.length > 12)
+            return false;
+
+        // calle vacia
+        if (!calle || calle.length === 0 || calle.length > 120)
+            return false;
+
+        // colonia vacia
+        if (!colonia || colonia.length === 0 || colonia.length > 60)
+            return false;
+
+        // numero exterior vacio
+        if (!numeroExterior || numeroExterior.length === 0 || numeroExterior.length > 6)
+            return false;
+
+        // numero exterior vacio
+        if (!CP || CP.length === 0 || CP.length > 5)
+            return false;
+
+        // estado invalido
+        if (!estado || estado == "-1" || !estados[estado])
+            return false;
+
+        // municipio invalido
+        if (!municipio || municipio == "-1" || !estados[estado].municipios[municipio])
+            return false;
+
+        // Contraseñas no coinciden
+        if (contrasena !== contrasenaC)
+            return false;
+
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
 export const register = async (req, res) => {
     try {
-        let newUser = req.body;
+        const newUser = req.body;
+
+        if (!validateUser(newUser))
+            return res.status(400).send("Datos invalidos");
 
         const salt = randomBytes(16).toString('hex');
         const hashedPassword = scryptSync(newUser.contrasena, salt, 64).toString('hex');
         newUser.contrasena = `${salt}:${hashedPassword}`;
         newUser.id = 0;
+        newUser.esAdmin = 0;
 
-
-        const [result] = await pool.query("SELECT * FROM usuarios WHERE usuario = ?", [newUser.usuario]);
-        if (result.length > 0) {
+        const [[usuario]] = await pool.query("SELECT * FROM usuarios WHERE usuario = ?", [newUser.usuario]);
+        const [[telefono]] = await pool.query("SELECT * FROM usuarios WHERE telefono = ?", [newUser.telefono]);
+        const [[correo]] = await pool.query("SELECT * FROM usuarios WHERE correo = ?", [newUser.correo]);
+        if (!usuario || !telefono || !correo) {
             res.status(400).send("Ya existe ese usuario");
         } else {
             await pool.query("INSERT INTO usuarios set ?", [newUser]);
