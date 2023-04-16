6d4ea2e08578a9be00a723f115d035ec3bb7b595
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-15 22:46:47 -0700

    Created calcularTotal Function

--- src/public/js/carrito.js
+++ src/public/js/carrito.js
@@ -119,6 +119,9 @@ function increaseItem(index) {
 		shoppingCart[index].cantidad += 1;
 	}
 
+	// Poner total correspondiente
+	document.querySelector("#shopping-cart-total").value = calcularTotal(shoppingCart);
+
 	// Cambiar valor de caja de texto
 	document.querySelectorAll(".item-counter")[index].value = shoppingCart[index].cantidad;
 
@@ -155,6 +158,9 @@ function decreaseItem(index) {
 		shoppingCart[index].cantidad -= 1;
 	}
 
+	// Poner total correspondiente
+	document.querySelector("#shopping-cart-total").value = calcularTotal(shoppingCart);
+
 	// Cambiar valor de caja de texto
 	document.querySelectorAll(".item-counter")[index].value = shoppingCart[index].cantidad;
 
@@ -185,6 +191,9 @@ function changeCantidadItem(index) {
 	} else if (shoppingCart[index].cantidad <= 0 || isNaN(shoppingCart[index].cantidad)) {
 		return deleteItem(index);
 	}
+
+	// Poner total correspondiente
+	document.querySelector("#shopping-cart-total").value = calcularTotal(shoppingCart);
 
 	document.querySelectorAll(".item-counter")[index].setAttribute("value", shoppingCart[index].cantidad);
 	document.querySelectorAll(".item-counter")[index].value = shoppingCart[index].cantidad;
@@ -251,6 +260,16 @@ async function addProductToCart(codigo) {
 	}
 }
 
+function calcularTotal(carrito = '[]') {
+	let total = 0;
+
+	carrito.forEach(item => {
+		total += parseFloat(item.precioFinal) * parseFloat(item.cantidad);
+	});
+
+	return total;
+}
+
 async function mostrarCarrito() {
 	// Obtener información del localStorage
 	let shoppingCart = localStorage.getItem("shopping-cart");
@@ -313,7 +332,6 @@ async function mostrarCarrito() {
 	// For para obtener informacion de cada item del inventario
 	let i = 0;
 	let erroneos = [];
-	let total = 0;
 	for (const item of shoppingCart) {
 		try {
 			let { data } = await axios.post('/productos/get', {
@@ -332,7 +350,7 @@ async function mostrarCarrito() {
 				item.cantidad = data.disponibilidad;
 			}
 			item.disponibilidad = data.disponibilidad;
-			total += parseFloat(data.precioFinal);
+			item.precioFinal = data.precioFinal;
 
 			tableContent.innerHTML += `<tr>
 				<td class="text-center p-0">
@@ -372,7 +390,7 @@ async function mostrarCarrito() {
 	}
 
 	// Poner total correspondiente
-	div.querySelector("#shopping-cart-total").value = total;
+	div.querySelector("#shopping-cart-total").value = calcularTotal(shoppingCart);
 
 	// Elimina los elementos en orden inverso para no afectar los indices
 	for (let i = erroneos.length - 1; i >= 0; i--) {
