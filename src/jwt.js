ae6acf84500dbefa76d63c1f5a20d21ff68b0127
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-16 11:32:01 -0700

    Moved user info from req.body to req.user

--- src/jwt.js
+++ src/jwt.js
@@ -48,9 +48,9 @@ export const validarJWT = async (req, res, next) => {
 		}
 
 		// Asignacion de valores
-		req.body.isLogged = true;
-		req.body.id = id;
-		req.body.esAdmin = user.esAdmin;
+		req.user.isLogged = true;
+		req.user.id = id;
+		req.user.esAdmin = user.esAdmin;
 
 		next();
 	} catch (error) {
@@ -66,8 +66,8 @@ export const checkLogged = async (req, res, next) => {
 
 	// Si no hay token deja avanzar al siguiente middleware
 	if (!token) {
-		req.body.isLogged = false;
-		req.body.esAdmin = 0;
+		req.user.isLogged = false;
+		req.user.esAdmin = 0;
 		return next();
 	}
 
@@ -84,13 +84,13 @@ export const checkLogged = async (req, res, next) => {
 			return next();
 		}
 
-		req.body.isLogged = true;
-		req.body.esAdmin = user.esAdmin;
+		req.user.isLogged = true;
+		req.user.esAdmin = user.esAdmin;
 
-		if (req.body.esAdmin === 1) {
+		if (req.user.esAdmin === 1) {
 			// Si es admin te redirecciona a el menu
 			return res.redirect('/admin/');
-		} else if (req.body.esAdmin === 0) {
+		} else if (req.user.esAdmin === 0) {
 			// Si no es admin te redirecciona a la seccion de clientes
 			return res.redirect('/');
 		}
@@ -107,7 +107,7 @@ export const checkLogged = async (req, res, next) => {
 export const validarAdmin = (req, res, next) => {
 	try {
 		// Obtiene isAdmin del body
-		const { esAdmin } = req.body;
+		const { esAdmin } = req.user;
 
 		// Si el rol es distinto a admin te manda un error
 		if (esAdmin !== 1) {
@@ -125,14 +125,14 @@ export const validarAdmin = (req, res, next) => {
 export const validarCliente = (req, res, next) => {
 	try {
 		// Obtiene esAdmin del body
-		const { esAdmin } = req.body;
+		const { esAdmin } = req.user;
 
 		if (esAdmin === 1) {
 			// Si es admin te da descuento
-			req.body.descuento = 10;
+			req.user.descuento = 10;
 		} else {
 			// Si no, no hay descuento
-			req.body.descuento = 0;
+			req.user.descuento = 0;
 		}
 
 		return next();
@@ -149,8 +149,8 @@ export const isLogged = async (req, res, next) => {
 
 	// Token no enviado
 	if (!token) {
-		req.body.isLogged = false;
-		req.body.esAdmin = 0;
+		req.user.isLogged = false;
+		req.user.esAdmin = 0;
 		return next();
 	}
 
@@ -168,9 +168,9 @@ export const isLogged = async (req, res, next) => {
 		}
 
 		// Asignacion de valores
-		req.body.isLogged = true;
-		req.body.id = id;
-		req.body.esAdmin = user.esAdmin;
+		req.user.isLogged = true;
+		req.user.id = id;
+		req.user.esAdmin = user.esAdmin;
 
 		next();
 	} catch (error) {
