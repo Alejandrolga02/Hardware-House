256fd75056549efef51cfd38c80817d62470b123
Alejandro Ivan Lizarraga Rojas <alejandroivanliza@gmail.com>
2023-04-17 21:30:16 -0700

    Add .map

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-import http from 'http';
-import https from 'https';
-import fs from 'fs';
-
 import app from "./app.js";
-import { PORT, SECURE_PORT, SSL_CA_PATH, SSL_CERT_PATH, SSL_KEY_PATH } from "./config.js";
+import { PORT } from "./config.js";
 
-https.createServer({
-	key: fs.readFileSync(SSL_KEY_PATH),
-	ca: fs.readFileSync(SSL_CA_PATH),
-	cert: fs.readFileSync(SSL_CERT_PATH),
-}, app).listen(SECURE_PORT, "alejandrolizarraga.tech");
-
-http.createServer((req, res) => {
-	res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-	res.end();
-}).listen(PORT);
-
-console.log(`Servidor con SSL en PUERTO ${SECURE_PORT}`);
+app.listen(PORT);
+console.log(`Servidor en puerto ${PORT}`);
